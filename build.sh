f17dcf55f20d6cc66ec93e633d2f775938705e1d
Xilong Yang <xilong.yang@foxmail.com>
2022-01-07 22:01:41 +0800

    complete info

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 ./scripts/pre_processor.py ./src/en.html > en.html
 ./scripts/pre_processor.py ./src/zh.html > zh.html
-./scripts/pre_processor.py ./src/404.html > 404.html
 ln -sf ./en.html ./index.html
