a1e1687f521c1cf6c9b350a3650021041627200a
Xilong Yang <contact@xilong.site>
2022-01-07 15:29:15 +0800

    add 404 page

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 ./scripts/pre_processor.py ./src/en.html > en.html
 ./scripts/pre_processor.py ./src/zh.html > zh.html
+./scripts/pre_processor.py ./src/404.html > 404.html
 ln -sf ./en.html ./index.html
