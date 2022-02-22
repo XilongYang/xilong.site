9fee3bb4c1396f25f4d1fc417deac71cab33cf27
Xilong Yang <xilong.yang@foxmail.com>
2022-02-22 16:21:24 +0800

    update

--- scripts/switchDisplay.js
+++ scripts/switchDisplay.js
@@ -1,8 +1,15 @@
-function switchDisplay(id) {
-    var target = document.getElementById(id);
+var target=null;
+function hide() {
+    target.style.display="none";
+};
+function switchDisplay(id, animation_in, animation_out) {
+    target = document.getElementById(id);
     if(target.style.display == "inline") {
-        target.style.display = "none";
+        target.addEventListener("animationend", hide);
+        target.style.animation=animation_out;
     } else {
+        target.removeEventListener("animationend", hide);
+        target.style.animation=animation_in;
         target.style.display = "inline";
     }
 }
