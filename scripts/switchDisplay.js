813d12c9dce3ac18266bd4352af4cb61166724ae
Xilong Yang <contact@xilong.site>
2022-02-22 20:18:35 +0800

    update

--- scripts/switchDisplay.js
+++ scripts/switchDisplay.js
@@ -18,3 +18,20 @@ function reloadFrame(id) {
     var frame = document.getElementById(id);
     frame.src = frame.src;
 }
+
+function switchDarkmode() {
+    var mask = document.getElementById("darkmode-mask");
+    if (mask.style.opacity != "1") {
+        mask.style.opacity = "1";
+    } else {
+        mask.style.opacity = "0";
+    }
+    var buttons = document.getElementsByClassName("fake_close");
+    for (var i = 0; i < buttons.length; ++i) {
+        buttons[i].style.filter = "invert(1)";
+    }
+    var frames = document.getElementsByClassName("fake_window_contents");
+    for (var i = 0; i < buttons.length; ++i) {
+        frames[i].style.filter = "invert(1)";
+    }
+}
