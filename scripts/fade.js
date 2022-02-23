d857da12572bbb4005a14089012fda9eb12e36a5
Xilong Yang <contact@xilong.site>
2022-02-24 06:52:51 +0800

    update

--- scripts/fade.js
+++ scripts/fade.js
@@ -2,11 +2,11 @@ var prePos = 642;
 function fade() {
     var fades = document.getElementById("fades");
     var curPos = fades.offsetTop - document.documentElement.scrollTop;
-    if (curPos < prePos && curPos < 400) {
+    if (curPos < prePos && curPos < 500) {
         fades.style.animation="fadein 1s";
         fades.style.opacity=1;
     }
-    if (curPos > prePos && curPos >= 400) {
+    if (curPos > prePos && curPos >= 500) {
         fades.style.animation="fadeout 1s";
         fades.style.opacity=0;
     }
