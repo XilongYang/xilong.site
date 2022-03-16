31bebea29e29b984d589e927c46fc2bdd1957447
Xilong Yang <contact@xilong.site>
2022-03-16 19:51:46 +0800

    update

--- scripts/animation.js
+++ scripts/animation.js
@@ -3,11 +3,15 @@ function fade() {
     var fades = document.getElementById("fades");
     var curPos = fades.offsetTop - document.documentElement.scrollTop;
     if (curPos < prePos && curPos < 500) {
-        fades.style.animation="fadein 1s";
+        if (windowsNums == 0) {
+            fades.style.animation="fadein 1s";
+        }
         fades.style.opacity=1;
     }
     if (curPos > prePos && curPos >= 500) {
-        fades.style.animation="fadeout 1s";
+        if (windowsNums == 0) {
+            fades.style.animation="fadeout 1s";
+        }
         fades.style.opacity=0;
     }
     prePos = curPos;
