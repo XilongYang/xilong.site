e727e88560524ebc1dd2abd31aa1fb0bf6ab5517
Xilong Yang <xilong.yang@foxmail.com>
2024-03-17 16:09:21 +0800

    update-index.py

--- build/update-index.py
+++ build/update-index.py
@@ -2,22 +2,62 @@
 
 import os
 import sys
-import subprocess
+import re
 
-ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-POST_PATH = ROOT_PATH + '/posts'
-TEMPLATE = ROOT_PATH + '/template/post.html'
+LOCAL_ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+LOCAL_SRC_PATH = LOCAL_ROOT_PATH + '/src'
+LOCAL_INDEX_PATH = LOCAL_ROOT_PATH + '/index.html'
+LOCAL_TEMPLATE_PATH = LOCAL_ROOT_PATH + '/template/index.html'
 
-SOURCE_SUFFIX = ".md"
-TARGET_SUFFIX = ".html"
+WEB_ROOT_PATH = ''
+WEB_POST_PATH= WEB_ROOT_PATH + '/posts'
 
-COMPILE_COMMAND = "pandoc {} -o {} --no-highlight --template={} --mathjax"
+POST_HTML_TEMPLATE = '<p>$date$ <a href="$link$">$title$</a></p>'
 
-def file2path(filename, suffix):
-    return POST_PATH + '/' + filename.rstrip('\n') + suffix
+def src2path(file_name):
+    return LOCAL_SRC_PATH + '/' + file_name.rstrip('\n') + '.md'
 
-for file in sys.stdin:
-    source_path = file2path(file, SOURCE_SUFFIX)
-    target_path = file2path(file, TARGET_SUFFIX)
-    subprocess.run(COMPILE_COMMAND.format(source_path, target_path, TEMPLATE), shell=True)
-    print("Build:" + file)
+def post2link(file_name):
+    return WEB_POST_PATH+ '/' + file_name.rstrip('\n') + '.html'
+
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+
+def parse_meta(src_file_path):
+    result = {}
+    parse_on = False
+    with open(src_file_path, "r") as file:
+        for line in file:
+            clear_line = line.replace('\n', '').replace(' ', '')
+            if clear_line == '---':
+                parse_on = not parse_on
+            elif parse_on:
+                parse = clear_line.split(':', 1)
+                result[parse[0]] = parse[1]
+    return result
+
+def post_html():
+    post_map = {} 
+    for src in list(set([file.split('.')[0] for file in os.listdir(LOCAL_SRC_PATH)])):
+        meta = parse_meta(src2path(src))
+        title = meta['title']
+        date = meta['date'].split('-', 1)
+        post_item = POST_HTML_TEMPLATE.replace("$date$", date[1]).replace("$link$", post2link(src)).replace("$title$", title)
+
+        if None == post_map.get(date[0]):
+            post_map[date[0]] = [post_item]
+        else:
+            post_map[date[0]].append(post_item)
+
+    post_html = ''
+    for year_post in sorted(list(post_map.items()), key=lambda x: x[0], reverse=True):
+        post_html += '<h3>' + year_post[0] + '</h3>' + '\n'
+        for post_items in sorted(year_post[1], reverse=True):
+            post_html += post_items + '\n'
+
+    return post_html
+
+index_contents = read_file(LOCAL_TEMPLATE_PATH).replace("$posts$", post_html())
+with open(LOCAL_INDEX_PATH, "w") as index_file:
+    index_file.write(index_contents)
