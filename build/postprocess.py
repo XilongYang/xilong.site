3038f1f6f5e8073a4ba7c5c877d4c86092d15e7f
Xilong Yang <contact@xilong.site>
2024-09-06 09:06:07 +0800

    Fix TOC space missing

--- build/postprocess.py
+++ build/postprocess.py
@@ -10,9 +10,8 @@ MAX_LEVEL = 4
 def part_toc(contents, level):
     if contents == '' or level > MAX_LEVEL:
         return ''
-    
-    contents = re.sub(r'\s', '', contents)
-    title_pattern = '<h%did=".*">.*</h%d>' % (level, level)
+     
+    title_pattern = '<h%d *id=".*">.*</h%d>' % (level, level)
     indexs = [i for i in range(0, len(contents)) if re.match(title_pattern, contents[i:]) != None]
     indexs.append(len(contents))
 
@@ -21,7 +20,6 @@ def part_toc(contents, level):
     parse_sub_contents = lambda i : contents[indexs[i]:indexs[i + 1]]
 
     item_pair = [(parse_id(i), parse_title(i), parse_sub_contents(i)) for i in range(len(indexs) - 1)]
-
     template = '<li><a href="#{}">{}</a>{}</li>\n'
 
     items = [template.format(x[0], x[1], part_toc(x[2], level + 1)) for x in item_pair]
@@ -33,12 +31,14 @@ def is_chinese_post(contents):
 
 def generate_toc(contents):
     toc_template = '<nav role="navigation" class="toc">\n<h2>Contents<i class="material-icons icon" id="toc-control">remove</i></h2>\n<div id="toc-items">\n{}\n</div>\n</nav>'
-    if is_chinese_post(contents):
+    if is_chinese_post(''.join(contents)):
         toc_template = toc_template.replace('Contents', '目录')
-    toc = toc_template.format(part_toc(contents, 2))
+    
+    serialized_contents = ''.join([x.strip() for x in contents]).replace('\n', '')
+    toc = toc_template.format(part_toc(serialized_contents, 2))
 
     return ''.join(contents).replace('<p>[[toc]]</p>', toc)
 
 def postprocess(src_file, target_file):
     # Generate TOC
-    write_file(target_file, generate_toc(''.join(read_file(src_file))))
+    write_file(target_file, generate_toc(read_file(src_file)))
