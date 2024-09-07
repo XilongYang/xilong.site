06f05432102c3220d9a7b72a72ae18b8f80f809f
Xilong Yang <xilong.yang@foxmail.com>
2024-09-07 20:03:48 +0800

    Add: dark mode for code block

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,10 +1,13 @@
 function switchDarkMode() {
     var isDark = document.body.classList.toggle("latex-dark");
     var icon = document.getElementById('darkmode');
+    var codeStyle = document.getElementById('code-style')
     if (isDark) {
         icon.innerText = 'light_mode';
+        codeStyle.href = '/res/latex-css-1.10.0/prism/prism-dracula.css';
     } else {
         icon.innerText = 'dark_mode';
+        codeStyle.href = '/res/latex-css-1.10.0/prism/prism-one-light.css';
     }
 }
 
