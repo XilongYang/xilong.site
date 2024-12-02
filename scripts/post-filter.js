3ed9df1b11dbe47604680715c286cad108cfaf5e
Xilong Yang <contact@xilong.site>
2024-12-02 22:35:00 +0800

    Complete: Post filter

--- scripts/post-filter.js
+++ scripts/post-filter.js
@@ -24,14 +24,21 @@ const pl     = [ST.INIT, ST.NORMAL  , EMP_FIELD, ST.NOT   , ST.PL           , EM
 const pr     = [ST.INIT, OPT_MISS   , ST.AND_OR, OPT_MISS , OPT_MISS        , ST.PR            , ST.ESC   ]
 const esc    = [ST.INIT, INVALID_ESC, ST.NORMAL, ST.NORMAL, ST.NORMAL       , ST.NORMAL        , ST.NORMAL]
 
-class JudgeNode {
-    exp;
-    successNext;
-    failureNext;
+class Ptr {
+    value
+    constructor(value) {
+        this.value = value
+    }
+}
+
+class MatchNode {
+    exp
+    successNext
+    failureNext
     constructor() {
         this.exp = ""
-        this.successNext = true
-        this.failureNext = false
+        this.successNext = new Ptr(new Ptr(true))
+        this.failureNext = new Ptr(new Ptr(false))
     }
 }
 
@@ -43,6 +50,7 @@ function toggleInput() {
         filterIcon.classList.remove("icon-active")
         input.style.display = "none"
         input.innerHTML = ""
+        refreshFilter()
     } else {
         filterIcon.classList.add("icon-active")
         filterIcon.classList.remove("icon")
@@ -90,9 +98,17 @@ function clearErrorMsg() {
 
 function refreshFilter() {
     var input = document.getElementById("post-filter-input")
-    var exp = input.innerText.replace("\n", "")
+    var exp = input.innerText.replace("\n", "").trim()
     if (exp == "") {
         input.innerHTML = ""
+        var postYears = document.getElementsByClassName('post-year-wrapper')
+        for (var postYear of postYears) {
+            var posts = postYear.getElementsByClassName('post-wrapper')
+            for (var post of posts) {
+                post.style.display = 'block'
+            }
+            postYear.style.display = 'block'
+        }
         return
     }
 
@@ -102,15 +118,21 @@ function refreshFilter() {
 
     var matchTree = expressionParse(exp)
 
+    var postYears = document.getElementsByClassName('post-year-wrapper')
+    for (var postYear of postYears) {
+        var atLeastOne = false
+        var posts = postYear.getElementsByClassName('post-wrapper')
+        for (var post of posts) {
+            var isSuccess = expressionMatch(matchTree, post.innerText)
+            atLeastOne = atLeastOne || isSuccess
+            post.style.display = isSuccess ? 'block' : 'none'
+        }
+        postYear.style.display = atLeastOne ? 'block' : 'none'
+    }
 
 }
 
 function expressionValidate(exp) {
-    exp = exp.trim()
-    if (exp == "") {
-        return false
-    }
-
     var states = [init, normal, andOr, not, pl, pr, esc]
     var curST = ST.INIT
 
@@ -171,9 +193,92 @@ function expressionValidate(exp) {
 }
 
 function expressionParse(exp) {
-    exp = exp.trim()
+    var stack      = []
+    var head       = new MatchNode()
+    var cur        = head
 
+    for (var i = 0; i < exp.length; ++i) {
+        var c = exp[i]
+        switch (c) {
+            case "&":
+                var next = new MatchNode()
+                if (cur.successNext.value.value == true) {
+                    next.failureNext = cur.failureNext
+                    next.successNext.value = cur.successNext.value
+                    cur.successNext.value = new Ptr(next)
+                } else {
+                    next.failureNext = cur.successNext
+                    next.successNext.value = cur.failureNext.value
+                    cur.failureNext.value = new Ptr(next)
+                }
+                cur = next
+                break;
+            case "|":
+                var next = new MatchNode()
+                if (cur.successNext.value.value == true) {
+                    next.successNext = cur.successNext
+                    next.failureNext.value = cur.failureNext.value
+                    cur.failureNext.value = new Ptr(next)
+                } else {
+                    next.successNext = cur.failureNext
+                    next.failureNext.value = cur.successNext.value
+                    cur.successNext.value = new Ptr(next)
+                }
+                cur = next
+                break
+            case "~":
+                var temp = cur.failureNext
+                cur.failureNext = cur.successNext
+                cur.successNext = temp
+                break
+            case "(":
+                stack.push([head, cur])
+                head = new MatchNode()
+                cur = head
+                break
+            case ")":
+                var target = stack.pop()
+                var head0  = target[0]
+                var cur0   = target[1]
 
+                cur.failureNext.value = cur0.failureNext.value
+                cur.successNext.value = cur0.successNext.value
+
+                cur0.failureNext = head.failureNext
+                cur0.successNext = head.successNext
+                cur0.exp = head.exp
+
+                head = head0
+                break
+            case "\\":
+                i++;
+                cur.exp += exp[i]
+                break
+            default:
+                cur.exp += c
+        }
+    }
+
+    return head
+}
+
+function expressionMatch(matchTree, str) {
+    var matchingExp = matchTree.exp.trim()
+    var isSuccess = str.includes(matchingExp)
+
+    if (isSuccess) {
+        var next = matchTree.successNext.value.value
+        if (typeof(next) == 'boolean') {
+            return next;
+        }
+        return expressionMatch(next, str)
+    }
+
+    var next = matchTree.failureNext.value.value
+    if (typeof(next) == 'boolean') {
+        return next;
+    }
+    return expressionMatch(next, str)
 }
 
 function errorMsg(msg, exp, index) {
