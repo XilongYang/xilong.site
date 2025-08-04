788e19fc480851f688539d1ddb818cf9744c25e2
Xilong Yang <xilong.yang@foxmail.com>
2025-08-04 22:31:32 +0900

    Restore font lazy-load to avoid the font-flash

--- scripts/init.js
+++ scripts/init.js
@@ -1,4 +1,5 @@
 import {refreshDarkMode, switchDarkMode} from "./darkmode.js"
+import {loadFont} from "./load-font.js"
 import {getCurrentYear} from "./datetime.js"
 import {switchTocMode} from "./toc.js"
 import {backTop, goBottom} from "./navigator.js"
@@ -6,6 +7,7 @@ import {closePanel, openPanel, search} from "./search.js"
 import {clearErrorMsg, inputFilter, refreshFilter, toggleInput} from "./post-filter.js"
 
 refreshDarkMode()
+loadFont()
 
 var darkmode = document.getElementById('darkmode')
 darkmode.addEventListener('click', switchDarkMode)
